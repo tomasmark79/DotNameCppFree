32d7879541b847f7f05fe3c3cb05327357041893
tomasmark79 <admin@digitalspace.name>
2025-05-14 13:41:24 +0200

    Committed by Pro Template 2025-05-14 13:41:24

--- cmake/tmplt-emscripten.cmake
+++ cmake/tmplt-emscripten.cmake
@@ -1,5 +1,9 @@
 # MIT License Copyright (c) 2024-2025 Tomáš Mark
 
+# SERVER SIDE DEPENDENCIS Apache directives example for CORS <IfModule mod_headers.c> Header set
+# Cross-Origin-Opener-Policy "same-origin" Header set Cross-Origin-Embedder-Policy "require-corp"
+# </IfModule>
+
 function(emscripten target isHtml reqPthreads customPrePath)
     if(CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
         message(STATUS "Emscripten environment detected")
@@ -17,9 +21,6 @@ function(emscripten target isHtml reqPthreads customPrePath)
         # available switches
         set(_wasm "-s WASM=1")
         set(_o3 "-O3")
-        set(_assertions "-s ASSERTIONS=1")
-        set(_asyncify "-s ASYNCIFY")
-        set(_gl_egpaddress "-s GL_ENABLE_GET_PROC_ADDRESS=1")
         set(_raylib "-s USE_GLFW=3")
         set(_sdl2_sdl2 "-s USE_SDL=2")
         set(_sdl2_image "-s USE_SDL_IMAGE=2")
@@ -27,10 +28,22 @@ function(emscripten target isHtml reqPthreads customPrePath)
         set(_sdl2_net "-s USE_SDL_NET=2")
         set(_sdl2_mixer "-s USE_SDL_MIXER=2")
 
+        # not used yet
+
+        # set(_gl_egpaddress "-s GL_ENABLE_GET_PROC_ADDRESS=1")
+
+        # set(_assertions "-s ASSERTIONS=1")
+
+        # set(_asyncify "-s ASYNCIFY")
+
+        # set(_memory_growth "-s ALLOW_MEMORY_GROWTH=1")
+
         if(reqPthreads EQUAL 1)
             set(_pthread "-s USE_PTHREADS=1 -pthread")
+            set(_pthread_pool_size "-s PTHREAD_POOL_SIZE=4")
         else()
             set(_pthread "")
+            set(_pthread_pool_size "")
         endif()
 
         # default assets direction is share/${target}/assets
@@ -38,13 +51,27 @@ function(emscripten target isHtml reqPthreads customPrePath)
             set(customPrePath "--preload-file ../../../../assets@share/${target}/assets")
         endif()
 
+        # custom assets path
+        if(target MATCHES "LibTester")
+            set(customHtmlPath
+                "--shell-file ${CMAKE_CURRENT_SOURCE_DIR}/../../assets/ems-mini.html")
+        else()
+            set(customHtmlPath
+                "--shell-file ${CMAKE_CURRENT_SOURCE_DIR}/../assets/ems-mini.html")
+        endif()
+
         # manually set flags is the best way
-        set_target_properties(${target} PROPERTIES COMPILE_FLAGS "${_o3} ${_pthread}")
+        set_target_properties(
+            ${target}
+            PROPERTIES
+                COMPILE_FLAGS
+                "${_o3} ${_pthread} ${_sdl2_sdl2} ${_sdl2_image} ${_sdl2_ttf} ${_sdl2_net} ${_sdl2_mixer} "
+        )
         set_target_properties(
             ${target}
             PROPERTIES
                 LINK_FLAGS
-                "${_wasm} ${_pthread} ${customPrePath}"
+                "${_wasm} ${_pthread} ${_pthread_pool_size} ${_sdl2_sdl2} ${_sdl2_image} ${_sdl2_ttf} ${_sdl2_net} ${_sdl2_mixer} ${customPrePath} ${customHtmlPath}"
         )
 
     endif()
